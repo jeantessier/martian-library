58eb712990767cc39f943757c3efb06e3cf052f2
Jean Tessier <jean.tessier@gusto.com>
2019-12-09 14:33:21 -0800

    Fix for style
    And provide an explicit description for the `me` query field.

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,18 +1,21 @@
 module Types
   class QueryType < Types::BaseObject
     field :items,
-          [Types::ItemType],
-          null: true,
-          description: "Returns a list of items in the martian library"do
+        [Types::ItemType],
+        null: true,
+        description: "Returns a list of items in the martian library"do
       argument :for_user, ID, 'The ID of the desired user', required: false
     end
     field :users,
-          [Types::UserType],
-          null: true,
-          description: "Returns a list of users of the martian library"do
+        [Types::UserType],
+        null: true,
+        description: "Returns a list of users of the martian library"do
       argument :user_id, ID, 'The ID of the desired user', required: false
     end
-    field :me, Types::UserType, null: true
+    field :me,
+        Types::UserType,
+        null: true,
+        description: "Returns the currently logged in user"
 
     def items(for_user: nil)
       (for_user ? Item.where(user_id: for_user) : Item.all).preload(:user)
